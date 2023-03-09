1f332e99dc94837f9f5cc033e37436f233940040
GenghisCahn <115155331+GenghisCahn@users.noreply.github.com>
2023-03-09 14:38:34 +0000

    initial commit
    commit initial files

---
+++ data.js
@@ -0,0 +1,13 @@
+// Product A info
+export let productA = {
+    emoji: "⭐",
+    revenue: 200,
+    commission: 50
+}
+
+// Product B info
+export let productB = {
+    emoji: "🔥",
+    revenue: 300,
+    commission: 75
+}
---
+++ index.js
@@ -0,0 +1,71 @@
+import {productA} from '/data.js'
+import {productB} from '/data.js'
+
+const salesDisplay = document.getElementById('sales-display')
+const achievementsDisplay = document.getElementById('achievements-display')
+const salesCounter = document.getElementById('sales-counter')
+const achievementsCounter = document.getElementById('achievements-counter')
+const revenueDisplay = document.getElementById('revenue-display')
+const commissionDisplay = document.getElementById('commission-display')
+
+let sales = []
+let achievements = []
+let totalRevenue = 0
+let totalCommission = 0
+let giveAchievement = true
+
+document.getElementById('star-btn').addEventListener('click',function(){
+    sales.push(productA.emoji)
+    salesRender()
+    achievementsRender()
+    revenueRender()
+    commissionRender()
+})
+
+
+document.getElementById('fire-btn').addEventListener('click',function(){ 
+    sales.push(productB.emoji)
+    salesRender()
+    achievementsRender()
+    revenueRender()
+    commissionRender()
+})
+
+
+function salesRender(){
+    salesDisplay.textContent = ''
+    for (let i = 0; i < sales.length; i++){
+        salesDisplay.textContent += sales[i]
+    }
+    
+    salesCounter.textContent = sales.length
+}
+
+
+function achievementsRender() {
+    achievementsDisplay.textContent = ''
+    for(let i = 0; i < achievements.length; i++){
+        achievementsDisplay.textContent += achievements[i]    
+    }
+    
+    achievementsCounter.textContent = achievements.length
+    
+      if(sales.length === 1){
+        achievements.push("🔔")
+    }else if(sales.length === 15){
+        achievements.push("🏆")
+    }else if(totalRevenue >= 2500 && giveAchievement){
+        achievements.push("💰")
+        giveAchievement = false
+    }
+}
+
+function revenueRender(){
+    totalRevenue += productA.revenue
+    revenueDisplay.textContent = "$ " + totalRevenue
+}
+
+function commissionRender(){
+    totalCommission += productA.commission
+    commissionDisplay.textContent = "$ " + totalCommission
+}
